4ad5eb65836d50edad249d58d737e7957e4c5826
Henry Gustavo X Az <azhenrygus@outlook.es>
2023-03-13 22:19:52 -0600

    cors policy modified

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const personsRouter = require('./routes/persons.routes.js')
+const cors = require('cors')
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req,res) => {
